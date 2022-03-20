73203675e0ad614f8462a91acd7c14c3c561fbbd
Monarch <vichukartha@gmail.com>
2022-03-20 21:35:20 +0530

    made pwa compatable2

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Installing service worker
 const CACHE_NAME = 'varsha';
 
-let resourcesToCache = ["./", "./index.html", "/service-worker.js", "./images/logo.jpg", "./images/bg.jpg", "./images/icon.ico", "./fonts/Quicksand-Bold.ttf", "./fonts/Quicksand-Light.ttf", "./fonts/Quicksand-Regular.ttf", "./manifest.json", "./style.css"];
+let resourcesToCache = ["./", "./index.html", "/service-worker.js", "./images/logo.jpg", "logo.png", "./images/bg.jpg", "./images/icon.ico", "./fonts/Quicksand-Bold.ttf", "./fonts/Quicksand-Light.ttf", "./fonts/Quicksand-Regular.ttf", "./manifest.json", "./style.css"];
 
 self.addEventListener("install", e => {
     e.waitUntil(
