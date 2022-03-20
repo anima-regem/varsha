e2e509833588d6c2c6981e501086157a9a6d3af7
Monarch <vichukartha@gmail.com>
2022-03-20 21:59:41 +0530

    edited scope and start_url

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Installing service worker
 const CACHE_NAME = 'varsha';
 
-let resourcesToCache = ["", "index.html", "service-worker.js", "images/logo.jpg", "images/logo.png", "images/bg.jpg", "images/icon.ico", "manifest.json", "style.css", "fonts/Quicksand-Bold.ttf", "fonts/Quicksand-Light.ttf", "fonts/Quicksand-Regular.ttf", ];
+let resourcesToCache = ["/varsha/", "index.html", "service-worker.js", "images/logo.jpg", "images/logo.png", "images/bg.jpg", "images/icon.ico", "manifest.json", "style.css", "fonts/Quicksand-Bold.ttf", "fonts/Quicksand-Light.ttf", "fonts/Quicksand-Regular.ttf", ];
 
 self.addEventListener("install", e => {
     e.waitUntil(
